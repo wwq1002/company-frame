5fb0393686ccc03c7c529140ff340e027c5bfc6f
wwq <xianwwq@163.com>
2022-05-08 00:44:09 +0800

    脚手架基础环境搭建 pull 出问题测试

---
+++ src/main/java/com/yingxue/lesson/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.yingxue.lesson.mapper;
+
+import com.yingxue.lesson.entity.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
---
+++ src/test/java/com/yingxue/lesson/CompanyFrameApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yingxue.lesson;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class CompanyFrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
