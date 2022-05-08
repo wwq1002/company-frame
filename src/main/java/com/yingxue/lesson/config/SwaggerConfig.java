9691488692c85b58e83fdb38ae43d78bb5108e51
wwq <xianwwq@163.com>
2022-05-08 12:30:06 +0800

    redis增加

---
+++ src/main/java/com/yingxue/lesson/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.yingxue.lesson.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: SwaggerConfig
+ * TODO:类文件简单描述
+ * @Author:
+ * @UpdateUser:
+ * @Version: 0.0.1
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Value("${swagger.enable}")
+    private boolean enable;
+
+    @Bean
+    public Docket createDocket(){
+        List<Parameter> par=new ArrayList<>();
+        ParameterBuilder accessTokenBuilder=new ParameterBuilder();
+        ParameterBuilder refreshTokenBuilder=new ParameterBuilder();
+        accessTokenBuilder.name("authorization").description("程序员自测的时候动态传输AccessToken 入口")
+                .modelRef(new ModelRef("String")).parameterType("header").required(false);
+        refreshTokenBuilder.name("refreshToken").description("程序员自测的时候动态传输RefreshToken 入口")
+                .modelRef(new ModelRef("String")).parameterType("header").required(false);
+        par.add(accessTokenBuilder.build());
+        par.add(refreshTokenBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yingxue.lesson.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(par)
+                .enable(enable);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("陕铁院教育后台权限管理系统实战")
+                .description("陕铁院教育后台权限管理系统后端接口文档")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+
+    }
+}
--- src/test/java/com/yingxue/lesson/CompanyFrameApplicationTests.java
+++ src/test/java/com/yingxue/lesson/CompanyFrameApplicationTests.java
@@ -1,17 +1,39 @@
 package com.yingxue.lesson;
 
 
+import com.yingxue.lesson.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.yingxue.lesson.service.RedisService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class CompanyFrameApplicationTests {
+public class CompanyFrameApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
+
+
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    public void testRedis(){
+        redisService.set("redisTest","redis test");
+        System.out.println(redisService.get("redisTest"));
+    }
+
+
 
 }
